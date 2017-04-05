9dc211a87b092b1ee315b7717f4b810b74691b16
h1tb1t <h1tb1t@noreply.github.com>
2017-04-05 21:38:24 +0100

    Release 0.1

---
+++ ehf/Form_main.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ehf
+{
+    partial class Form_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.listBox_attachments = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_email.Location = new System.Drawing.Point(12, 12);
+            this.textBox_email.MaxLength = 0;
+            this.textBox_email.Multiline = true;
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_email.Size = new System.Drawing.Size(1398, 380);
+            this.textBox_email.TabIndex = 0;
+            this.textBox_email.Click += new System.EventHandler(this.textBox_email_Click);
+            this.textBox_email.TextChanged += new System.EventHandler(this.textBox_email_TextChanged);
+            // 
+            // listBox_attachments
+            // 
+            this.listBox_attachments.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox_attachments.FormattingEnabled = true;
+            this.listBox_attachments.ItemHeight = 25;
+            this.listBox_attachments.Location = new System.Drawing.Point(12, 398);
+            this.listBox_attachments.Name = "listBox_attachments";
+            this.listBox_attachments.Size = new System.Drawing.Size(1398, 204);
+            this.listBox_attachments.TabIndex = 1;
+            this.listBox_attachments.DoubleClick += new System.EventHandler(this.listBox_attachments_DoubleClick);
+            // 
+            // Form_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1422, 614);
+            this.Controls.Add(this.listBox_attachments);
+            this.Controls.Add(this.textBox_email);
+            this.Name = "Form_main";
+            this.ShowIcon = false;
+            this.Text = "ehf";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_main_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_email;
+        private System.Windows.Forms.ListBox listBox_attachments;
+    }
+}
+
---
+++ ehf/Form_main.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ehf
+{
+    public partial class Form_main : Form
+    {
+        List<file_attachment> fas = new List<file_attachment>();
+
+        public Form_main()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_email_TextChanged(object sender, EventArgs e)
+        {
+            listBox_attachments.Items.Clear();
+            fas.Clear();
+            foreach (string attachment in textBox_email.Text.Split(new string[] { "Content-Disposition: attachment" }, StringSplitOptions.None))
+            {
+                if (attachment.Contains("filename=\""))
+                {
+                    if (attachment.Contains("Content-Transfer-Encoding: base64"))
+                    {
+                        string fname = attachment.Split(new string[] { "filename=\"" }, StringSplitOptions.None)[1].Split('\"')[0];
+                        string b64 = Regex.Split(attachment, @"(\r\n){2}", RegexOptions.ExplicitCapture)[1].Split('-')[0];
+                        fas.Add(new file_attachment { filename = fname, base64 = b64 });
+                        listBox_attachments.Items.Add(fname);
+                    }
+                }
+            }
+        }
+
+        private void listBox_attachments_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox_attachments.SelectedIndex != -1)
+            {
+                File.WriteAllBytes(Path.GetTempPath()+fas[listBox_attachments.SelectedIndex].filename,Convert.FromBase64String(fas[listBox_attachments.SelectedIndex].base64));
+                Process proc = Process.Start(Path.GetTempPath() + fas[listBox_attachments.SelectedIndex].filename);
+            }
+        }
+
+        private void Form_main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (file_attachment fa in fas)
+            {
+                if (File.Exists(Path.GetTempPath() + fa.filename))
+                {
+                    File.Delete(Path.GetTempPath() + fa.filename);
+                }
+            }
+        }
+
+        private void textBox_email_Click(object sender, EventArgs e)
+        {
+            textBox_email.SelectAll();
+        }
+    }
+
+    public class file_attachment
+    {
+        public string filename { get; set; }
+        public string base64 { get; set; }
+    }
+}
